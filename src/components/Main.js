80151797a5cb315398c1d29baf1de48da7bb4f24
Merwyn Patrick <merwyn.cruz@gmail.com>
2022-06-06 14:27:37 -0400

    update main css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import './styles/main.css';
+import personal from '../images/personal2.png'
 
 function Main() {
   return (
@@ -9,14 +10,20 @@ function Main() {
           <h1 className="main-name">Merwyn Patrick</h1>
           <div className="main-title">
             <div className="title-wrapper">
-            <div className="title-item">Web Developer</div>
+            <div className="title-item">Developer</div>
             <div className="title-item">Mechanical Engineer</div>
             <div className="title-item">Stock Trader</div>
             </div>
           </div>
+          <div className="desc">
+            I develop web applications using the PERN stack (PostgreSQL, Express, React and Node.js). I am looking to transition my career to Web development full time.
+          </div>
         </div>
       </div>
-      <div className="main-right">Right</div>
+      <div className="main-right">
+        <div className="img-bg"></div>
+        <img src={personal} className="personal-image" alt="Personal Image" />
+      </div>
     </div>
   );
 }
